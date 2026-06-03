9ba92d3216e80b307570ae6189a9a0d5eccc9a07
Covenant Odion-Ehigiator <mccore.covenant@gmail.com>
2026-06-03 05:51:12 +0100

    Add Railway Firebase environment variables

--- app.py
+++ app.py
@@ -22,18 +22,18 @@ app = Flask(__name__)
 # --- FIREBASE CONNECTOR CONFIGURATION ---
 try:
     firebase_config = {
-        "type": os.environ.get("type"),
-        "project_id": os.environ.get("project_id"),
-        "private_key_id": os.environ.get("private_key_id"),
-        "private_key": os.environ.get("private_key").replace("\\n", "\n"),
-        "client_email": os.environ.get("client_email"),
-        "client_id": os.environ.get("client_id"),
-        "auth_uri": os.environ.get("auth_uri"),
-        "token_uri": os.environ.get("token_uri"),
-        "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
-        "client_x509_cert_url": os.environ.get("client_x509_cert_url"),
-        "universe_domain": os.environ.get("universe_domain")
-    }
+    "type": "service_account",
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
+}
 
     cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
